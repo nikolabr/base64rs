852a5840679091141588cfb84818e2c91c6b4d9f
nikolabr <nikolabrk@protonmail.com>
2021-11-06 17:40:33 +0100

    Add base64 decoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 pub mod base64 {
-    static BASE64_TABLE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    static BASE64_TABLE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    fn get_ascii(ch: u8) -> u8 {
+        match BASE64_TABLE.chars().position(|x| x == ch as char) {
+            None => 0,
+            Some(n) => n as u8
+        }
+    }
 
     fn get_b64_char(val: u8) -> char { 
         match BASE64_TABLE.chars().nth((val & 0x3F) as usize) { 
@@ -8,14 +15,14 @@ pub mod base64 {
         }
     }
 
-    pub fn is_valid_b64(data: String) -> bool { 
+    pub fn is_valid_b64(data: &String) -> bool { 
         if (data.len() == 0) || (data.len() % 4 != 0) { 
             return false
         }
         else { 
             let char_is_b64 = |x| { (0x30..=0x39).contains(x) || (0x41..=0x90).contains(x) 
                 || (0x61..=0x80).contains(x) || *x == 0x2F || *x == 0x2B || *x == 0x3D };
-            return data.into_bytes().iter().all(char_is_b64)
+            return data.as_bytes().iter().all(char_is_b64)
         }
 
     }
@@ -52,6 +59,20 @@ pub mod base64 {
 
     }
 
+    fn decode_chunk(chunk: &[u8]) -> Vec<u8>{
+        let mut tmp : Vec<u8> = Vec::new();
+
+        tmp.push((get_ascii(chunk[0]) << 2) | (get_ascii(chunk[1]) >> 4));
+        if chunk[2] != '=' as u8 {
+            tmp.push((get_ascii(chunk[1]) << 4) | (get_ascii(chunk[2]) >> 2));
+            if chunk[3] != '=' as u8 {
+                tmp.push((get_ascii(chunk[2]) << 6) | get_ascii(chunk[3]));
+            }
+        }
+
+        tmp
+    }
+
     pub fn encode(data: &Vec<u8>) -> String {
         let mut res = String::new();
         let chunks = data.chunks_exact(3);
@@ -62,6 +83,20 @@ pub mod base64 {
         }
         if !remainder.is_empty() { 
             res.extend(add_padding(remainder));
+        }
+
+        res
+    }
+
+    pub fn decode(data: &String) -> Vec<u8> {
+        let mut res: Vec<u8> = Vec::new();
+        if is_valid_b64(data) == true {
+            let bytes = data.as_bytes();
+            let chunks = bytes.chunks(4);
+
+            for chunk in chunks {
+                res.extend(decode_chunk(chunk)); 
+            }
         }
 
         res
--- tests/test.rs
+++ tests/test.rs
@@ -1,32 +1,45 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use base64::base64;
 
     #[test] 
     fn test_encode(){
         let text = String::from("Many hands make light work.");
-        assert_eq!(base64::base64::encode(&text.into_bytes()), "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
-        assert_eq!(base64::base64::encode(&String::from("").into_bytes()), "");
+        assert_eq!(base64::encode(&text.into_bytes()), "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
+        assert_eq!(base64::encode(&String::from("").into_bytes()), "");
+    }
+
+    #[test] 
+    fn test_decode(){
+        let text = String::from("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
+        assert_eq!(base64::decode(&text), String::from("Many hands make light work.").into_bytes());
+        assert_eq!(base64::decode(&"AAAAAAA".to_string()), vec![]);
+
+    }
+    #[test]
+    fn test_encode_decode(){  
+        let bytes = String::from("Many hands make light work.").into_bytes();
+        assert_eq!(base64::decode(&base64::encode(&bytes)), bytes);
     }
 
     #[test] 
     fn test_valid_ascii(){ // Test an ascii string
-        assert_eq!(base64::base64::is_valid_b64("    ".to_string()), false);
-        assert_eq!(base64::base64::is_valid_b64("\r\r\r\r".to_string()), false);
-        assert_eq!(base64::base64::is_valid_b64("\n\n\n\n".to_string()), false);
-        assert_eq!(base64::base64::is_valid_b64("".to_string()), false);
-        assert_eq!(base64::base64::is_valid_b64("\0".to_string()), false);
+        assert_eq!(base64::is_valid_b64(&"    ".to_string()), false);
+        assert_eq!(base64::is_valid_b64(&"\r\r\r\r".to_string()), false);
+        assert_eq!(base64::is_valid_b64(&"\n\n\n\n".to_string()), false);
+        assert_eq!(base64::is_valid_b64(&"".to_string()), false);
+        assert_eq!(base64::is_valid_b64(&"\0".to_string()), false);
     }
 
     #[test] 
     fn test_valid_b64(){ // Test a base64 string
-        assert_eq!(base64::base64::is_valid_b64("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu".to_string()), true);
-        assert_eq!(base64::base64::is_valid_b64("Y3J1c3RhY2Vhbg==".to_string()), true);
+        assert_eq!(base64::is_valid_b64(&"TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu".to_string()), true);
+        assert_eq!(base64::is_valid_b64(&"Y3J1c3RhY2Vhbg==".to_string()), true);
     }
 
     #[test]
     fn test_valid_utf8(){ // Test unicode
-        assert_eq!(base64::base64::is_valid_b64("Россия".to_string()), false);   
-        assert_eq!(base64::base64::is_valid_b64("日本国".to_string()), false);   
+        assert_eq!(base64::is_valid_b64(&"Россия".to_string()), false);   
+        assert_eq!(base64::is_valid_b64(&"日本国".to_string()), false);   
     }
 }
